4c0d17c796b5bfc1d6e5b88a1be513ff01fe644a
ahpercival <ahpercival@gmail.com>
2022-02-03 12:16:31 +0000

    create Progress Bar that is controlled by variable

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+  let value = 100;
+</script>
+
+<div class="progress-bar">
+  <div class="progress-status" style="width: {value}%" />
+</div>
+
+<style>
+  .progress-bar {
+    height: 5px;
+    width: 100%;
+  }
+  .progress-status {
+    height: 100%;
+    background-color: hsla(173, 56%, 44%, 1);
+    transition: width 0.5s;
+  }
+</style>
