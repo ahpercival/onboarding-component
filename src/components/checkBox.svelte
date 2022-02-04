3c4aa2f5062ca330e83fd61299af75c97f7669f8
ahpercival <ahpercival@gmail.com>
2022-02-04 11:30:46 +0000

    change color of checklist labels

--- src/components/checkBox.svelte
+++ src/components/checkBox.svelte
@@ -4,10 +4,20 @@
   export let text = "";
 </script>
 
-<div>
-  <label>
+<div class="checklist">
+  <label class="checklist-label">
     <input type="checkbox" bind:checked />
     {label}
   </label>
   <p>{text}</p>
 </div>
+
+<style>
+  .checklist {
+    margin: auto 1rem;
+  }
+
+  .checklist-label {
+    color: hsla(173, 56%, 44%, 1);
+  }
+</style>
