8ec94c18d54173845e898969fb2c16b65f618c67
ahpercival <ahpercival@gmail.com>
2022-02-04 09:17:31 +0000

    extract Checkbox into separate component file

---
+++ src/components/checkBox.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let checked = false;
+</script>
+
+<div>
+  <label>
+    <input type="checkbox" bind:checked />
+    {checked}
+  </label>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,21 @@
 <script>
   import ProgressBar from "../components/progressBar.svelte";
+  import CheckBox from "../components/checkBox.svelte";
   let progressState = [false, false];
+
   $: completedItems = progressState.filter((b) => {
     if (!!b) {
       return b;
     }
   });
+
   $: value = (100 / progressState.length) * completedItems.length;
   $: disabled = value !== 100;
 </script>
 
 <ProgressBar {value} />
-{#each progressState as bool}
-  <div>
-    <label>
-      <input type="checkbox" bind:checked={bool} />
-      {bool}
-    </label>
-  </div>
+{#each progressState as checked}
+  <CheckBox bind:checked />
 {/each}
 
 <button {disabled}>Continue</button>
