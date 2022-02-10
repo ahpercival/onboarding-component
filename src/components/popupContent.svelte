8ad79c4cbbcaf85f08376ea56f7fff79b8c0bc98
ahpercival <ahpercival@gmail.com>
2022-02-10 11:18:17 +0000

    extract popup content into separate component

---
+++ src/components/popupContent.svelte
@@ -0,0 +1,38 @@
+<script>
+  import CheckBox from "../components/checkBox.svelte";
+  export let display;
+</script>
+
+<div class="progress-popup">
+  <h3>Welcome To Beyonk</h3>
+  <div class="form-container" style="display: {display};">
+    <div class="checklist-container">
+      <p>
+        Selling tickets has never been easier. Follow our simple checklist below
+        to finish your set-up with Beyonk.
+      </p>
+      <CheckBox />
+    </div>
+  </div>
+</div>
+
+<style>
+  .progress-popup {
+    z-index: 9;
+    position: fixed;
+    bottom: 0;
+    right: 15px;
+    background-color: #f4f5f7;
+    font-family: "Cabin", "Trebuchet MS", Roboto, Helvetica, Arial, sans-serif;
+    color: hsla(0, 0%, 40%, 1);
+  }
+
+  .form-container {
+    max-width: 500px;
+    padding: 5px 10px;
+  }
+
+  .checklist-container {
+    padding: 0 3rem 1rem;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
   import ProgressBar from "../components/progressBar.svelte";
-  import CheckBox from "../components/checkBox.svelte";
   import LaunchButton from "../components/launchButton.svelte";
+  import PopupContent from "../components/popupContent.svelte";
 
   let display = "none";
 
@@ -14,39 +14,9 @@
   };
 </script>
 
-<div class="progress-popup">
+<div>
   <ProgressBar />
-  <h3>Welcome To Beyonk</h3>
-  <div class="form-container" style="display: {display};">
-    <div class="checklist-container">
-      <p>
-        Selling tickets has never been easier. Follow our simple checklist below
-        to finish your set-up with Beyonk.
-      </p>
-      <CheckBox />
-    </div>
-  </div>
+  <PopupContent {display} />
 </div>
 
 <LaunchButton {handlePopUp} />
-
-<style>
-  .progress-popup {
-    z-index: 9;
-    position: fixed;
-    bottom: 0;
-    right: 15px;
-    background-color: #f4f5f7;
-    font-family: "Cabin", "Trebuchet MS", Roboto, Helvetica, Arial, sans-serif;
-    color: hsla(0, 0%, 40%, 1);
-  }
-
-  .form-container {
-    max-width: 500px;
-    padding: 5px 10px;
-  }
-
-  .checklist-container {
-    padding: 0 3rem 1rem;
-  }
-</style>
