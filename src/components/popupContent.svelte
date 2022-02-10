828368ffd01995ba9991db1b077c00698eb72ac7
ahpercival <ahpercival@gmail.com>
2022-02-10 11:26:24 +0000

    style popup content to match mockup design

--- src/components/popupContent.svelte
+++ src/components/popupContent.svelte
@@ -1,38 +1,24 @@
 <script>
   import CheckBox from "../components/checkBox.svelte";
-  export let display;
 </script>
 
-<div class="progress-popup">
+<div class="checklist-container">
   <h3>Welcome To Beyonk</h3>
-  <div class="form-container" style="display: {display};">
-    <div class="checklist-container">
-      <p>
-        Selling tickets has never been easier. Follow our simple checklist below
-        to finish your set-up with Beyonk.
-      </p>
-      <CheckBox />
-    </div>
-  </div>
+  <p>
+    Selling tickets has never been easier. Follow our simple checklist below to
+    finish your set-up with Beyonk.
+  </p>
+  <CheckBox />
 </div>
 
 <style>
-  .progress-popup {
-    z-index: 9;
-    position: fixed;
-    bottom: 0;
-    right: 15px;
-    background-color: #f4f5f7;
-    font-family: "Cabin", "Trebuchet MS", Roboto, Helvetica, Arial, sans-serif;
-    color: hsla(0, 0%, 40%, 1);
-  }
-
-  .form-container {
-    max-width: 500px;
-    padding: 5px 10px;
-  }
-
   .checklist-container {
-    padding: 0 3rem 1rem;
+    padding: 0 1rem 2rem;
+  }
+
+  @media only screen and (max-width: 600px) {
+    .checklist-container {
+      padding: 0.5rem;
+    }
   }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,9 +14,37 @@
   };
 </script>
 
-<div>
-  <ProgressBar />
-  <PopupContent {display} />
+<div class="form-container">
+  <div class="progress-popup" style="display: {display};">
+    <ProgressBar />
+    <PopupContent />
+  </div>
 </div>
 
 <LaunchButton {handlePopUp} />
+
+<style>
+  .form-container {
+    z-index: 9;
+    position: fixed;
+    bottom: 6rem;
+    width: 100%;
+    margin: 0;
+  }
+
+  .progress-popup {
+    max-width: 500px;
+    background-color: hsla(224, 58%, 96%, 1);
+    font-family: "Cabin", "Trebuchet MS", Roboto, Helvetica, Arial, sans-serif;
+    color: hsla(0, 0%, 14%, 1);
+    box-shadow: 2px 5px 5px lightgrey;
+    margin: 0 auto;
+  }
+
+  @media only screen and (max-width: 600px) {
+    .progress-popup {
+      max-width: 90%;
+      margin: 0 auto;
+    }
+  }
+</style>
