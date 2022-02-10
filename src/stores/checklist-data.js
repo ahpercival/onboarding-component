3262d2b191c0677a7400fc1cac75b97f3c44c3fb
ahpercival <ahpercival@gmail.com>
2022-02-10 14:10:47 +0000

    update key names in checklistData object

--- src/stores/checklist-data.js
+++ src/stores/checklist-data.js
@@ -1,22 +1,22 @@
 import { readable } from "svelte/store";
 
 let checklistData = {
-  one: {
+  organisation: {
     checked: false,
     label: "Complete your company profile",
     text: "Upload your logo, pin your location, add a phone and description and you're all set!",
   },
-  two: {
+  adventure: {
     checked: false,
     label: "Create your first experience",
     text: "Follow the easy step-by-step guide to create a compelling experience that sells.",
   },
-  three: {
+  payment: {
     checked: false,
     label: "Link your payment account",
     text: "Connect via Stripe to get paid when you sell tickets.",
   },
-  four: {
+  policies: {
     checked: false,
     label: "Set your cancellation policy",
     text: "Choose between 'Flexible', 'Moderate' or set a Custom policy",
