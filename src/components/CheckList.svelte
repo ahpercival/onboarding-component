03113b9a3147f5f298a98828d5fc36607ae0c3b7
ahpercival <ahpercival@gmail.com>
2022-02-17 16:53:29 +0000

    add accordian to checklist

--- src/components/CheckList.svelte
+++ src/components/CheckList.svelte
@@ -1,5 +1,6 @@
 <script>
   import { progressStore } from "../stores.js";
+  import { Accordion, AccordionItem } from 'svelte-collapsible'
 
   const checklistData = [
     {
@@ -30,21 +31,30 @@
 </script>
 
 {#each checklistData as { key, label, text, url }}
-  <div>
-    <label class="checklist-label" class:checked={$progressStore[key]}>
-      <input class="checkbox" type="checkbox" />
-      {#if $progressStore[key]}
-        <span class="checkbox-icon" class:tick={$progressStore[key]}>✓</span>
-        {:else}
-        <span class="checkbox-icon">x</span>
-      {/if}
-      <a rel="prefetch" title={label} href={url}>{label}</a>
-    </label>
-    <p class="checklist-text">{text}</p>
+  <div class="checkbox-item">
+    <Accordion>
+      <label class="checklist-label" class:checked={$progressStore[key]}>
+        <input class="checkbox" type="checkbox" />
+        {#if $progressStore[key]}
+          <span class="checkbox-icon" class:tick={$progressStore[key]}>✓</span>
+          {:else}
+          <span class="checkbox-icon">x</span>
+        {/if}
+        <a rel="prefetch" title={label} href={url}>{label}</a>
+      </label>
+      <AccordionItem key={key}>
+        <span slot="header" class="dropdown-trigger"></span>
+        <p slot="body" class="checklist-text">{text}</p>
+      </AccordionItem>
+    </Accordion>
   </div>
 {/each}
 
 <style>
+  .checkbox-item {
+    margin: 1rem 0;
+  }
+  
   .checklist-label a {
     color: hsla(224, 59%, 53%, 1);
     text-decoration: none;
@@ -64,11 +74,33 @@
     color: hsla(0, 0%, 40%, 1);
   }
 
+  .dropdown-trigger {
+    float: right;
+    margin: 0 1rem;
+    color: hsla(0, 0%, 40%, 1);
+    cursor: pointer;
+  }
+
+  .dropdown-trigger::before {
+    border-style: solid;
+    border-width: 0.12em 0.12em 0 0;
+    content: '';
+    display: inline-block;
+    height: 0.45em;
+    left: 0.15em;
+    position: relative;
+    vertical-align: top;
+    width: 0.45em;
+    top: -15px;
+    transform: rotate(135deg);
+  }
+
   .tick {
     color: hsla(173, 56%, 44%, 1);
   }
   
   .checklist-text {
     color: hsla(0, 0%, 48%, 1);
+    margin: 0 0.5rem;
   }
 </style>
