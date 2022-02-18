42fc5cca68e3ec60e681c4a256785f18b39154b6
ahpercival <ahpercival@gmail.com>
2022-02-18 15:54:09 +0000

    refactor accordian

--- src/components/CheckList.svelte
+++ src/components/CheckList.svelte
@@ -1,58 +1,64 @@
 <script>
   import { progressStore } from "../stores.js";
-  import { Accordion, AccordionItem } from 'svelte-collapsible'
-
+  import { slide } from "svelte/transition";
   const checklistData = [
     {
       key: "organisation",
       label: "Complete your company profile",
       text: "Upload your logo, pin your location, add a phone and description and you're all set!",
-      url: "profile/organisation?backTo=/"
+      url: "profile/organisation?backTo=/",
+      toggle: false
     },
     {
       key: "adventure",
       label: "Create your first experience",
       text: "Follow the easy step-by-step guide to create a compelling experience that sells.",
-      url: "event/create?backTo=/"
+      url: "event/create?backTo=/",
+      toggle: false
     },
     {
       key: "payment",
       label: "Link your payment account",
       text: "Connect via Stripe to get paid when you sell tickets.",
-      url: "profile/payment-details?backTo=/"
+      url: "profile/payment-details?backTo=/",
+      toggle: false
     },
     {
       key: "policies",
       label: "Set your cancellation policy",
       text: "Choose between 'Flexible', 'Moderate' or set a Custom policy",
-      url: "profile/terms?backTo=/"
+      url: "profile/terms?backTo=/",
+      toggle: false
     },
   ];
 </script>
 
-{#each checklistData as { key, label, text, url }}
-  <div class="checkbox-item">
-    <Accordion>
-      <label class="checklist-label" class:checked={$progressStore[key]}>
-        <input class="checkbox" type="checkbox" />
+{#each checklistData as { key, label, text, url, toggle }}
+  <div class="checkbox-item" class:toggled={toggle}>
+    <label class="checklist-label" class:checked={$progressStore[key]}>
+      <input class="checkbox" type="checkbox" />
         {#if $progressStore[key]}
           <span class="checkbox-icon" class:tick={$progressStore[key]}>✓</span>
           {:else}
           <span class="checkbox-icon">x</span>
         {/if}
         <a rel="prefetch" title={label} href={url}>{label}</a>
-      </label>
-      <AccordionItem key={key}>
-        <span slot="header" class="dropdown-trigger"></span>
-        <p slot="body" class="checklist-text">{text}</p>
-      </AccordionItem>
-    </Accordion>
+    </label>
+    <span class="dropdown-trigger" on:click={()=>{toggle = !toggle}}></span>
+    {#if toggle}
+      <p transition:slide={{ duration: 300 }} class="checklist-text">{text}</p>
+    {/if}
   </div>
 {/each}
 
 <style>
   .checkbox-item {
-    margin: 1rem 0;
+    margin: 0.5rem 0;
+    padding: 0.5rem 0;
+  }
+
+  .toggled {
+    background-color: hsla(224, 58%, 96%, 1);
   }
   
   .checklist-label a {
@@ -91,7 +97,7 @@
     position: relative;
     vertical-align: top;
     width: 0.45em;
-    top: -15px;
+    top: 0;
     transform: rotate(135deg);
   }
 
@@ -103,5 +109,6 @@
     color: hsla(0, 0%, 48%, 1);
     margin: 0 0.5rem;
     width: 100%;
+    padding: 1rem 0;
   }
 </style>
